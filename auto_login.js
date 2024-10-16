9856a0bbba3c02e568cc02f4be984294224bf31e
Sam5440 <1102566608@qq.com>
2024-10-16 08:27:21 +0800

    自动登录的js脚本

---
+++ auto_login.js
@@ -0,0 +1,20 @@
+// ios快捷
+
+var result = [];
+
+var username = 'demo'; // 改成你的username
+var password = 'qwe123'; // 改成你的password
+
+// 填写用户名和密码
+var usernameInput = document.getElementById('username'); // 注意和网页的标签名保持一致
+var passwordInput = document.getElementById('password');
+
+usernameInput.value = username;
+passwordInput.value = password;
+
+// 点击登陆
+var loginButton = document.getElementById('login'); // 注意和网页的button名保持一致
+loginButton.click();
+
+
+completion(result);
